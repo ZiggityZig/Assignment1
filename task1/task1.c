8680b1cb8cfca27fc99de9935fb4cfe23c986f6c
omergv11 <gevao@post.bgu.ac.il>
2021-04-16 16:44:09 +0300

    change1

--- task1/task1.c
+++ task1/task1.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void assFunc(int x);
+extern void assFunc(int);
 
 char c_checkValidity(int x) {
-    printf("validity was checked");
     char ret = 0;
     if(x%2 == 0)
         ret = 1;
@@ -19,6 +18,8 @@ int main(int argc, char** argv) {
     printf("Please enter an integer number:\n");
     fgets(buffer, sizeof(buffer),stdin);
     sscanf(buffer, "%d", &x);
+    printf("___\n");
+
     assFunc(x);
     return 0;
 }
