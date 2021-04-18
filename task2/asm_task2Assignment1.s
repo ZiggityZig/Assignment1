cc23d225ad3bbd9861e4ce9c805f4a8876448272
ZiggityZig <zigi531@gmail.com>
2021-04-18 15:32:57 +0300

    Finished Writing assembly functio, still not debbuged

--- task2/asm_task2Assignment1.s
+++ task2/asm_task2Assignment1.s
@@ -2,12 +2,15 @@ section	.rodata			; we define (global) read-only variables in .rodata section
 	format_string: db "%s", 10, 0	; format string
 
 section .bss			; we define (global) uninitialized variables in .bss section
-	an: resb 12		; enough to store integer in [-2,147,483,648 (-2^31) : 2,147,483,647 (2^31-1)]
+	an: resb 33		; enough to store integer in [-2,147,483,648 (-2^31) : 2,147,483,647 (2^31-1)]
 	num: resd 1
 
 section .text
 	global convertor
 	extern printf
+
+section .data
+	index: dd 0
 
 convertor:
 	push ebp
@@ -15,17 +18,37 @@ convertor:
 	pushad			
 
 	mov ecx, dword [ebp+8]	; get function argument (pointer to string)
-
+	push an
 	findNum:
-		let:
-		cmp ecx,'A'
-
-		
-		num:
-
-		
-
-
+		movzx [num],ecx
+		cmp byte [num],'A'
+		jb dig
+		sub byte [num],'A'
+		add byte [num],10
+		jmp convert
+		dig:
+		sub byte [num],'0'
+	convert:
+		mov edx, 8
+		loop:
+		mov dword ebx, [num]
+		and ebx, edx
+		cmp ebx,0
+		jnz one
+		mov byte [an], '0'
+		jmp cont
+		one:
+		mov byte [an], '1'
+		cont:
+		inc an
+		shr edx,1
+		cmp edx, 1
+		jnz loop
+	inc ecx
+	cmp byte [ecx], 0
+	jnz findNum
+	mov byte [an], 0
+	pop an
 	push an			; call printf with 2 arguments -  
 	push format_string	; pointer to str and pointer to format string
 	call printf
