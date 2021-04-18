2e6ea43cefc4b53d2a73e64d9410fc3ae59cbe07
omergv11 <gevao@post.bgu.ac.il>
2021-04-18 18:50:48 +0300

    commit

--- task2/asm_task2Assignment1.s
+++ task2/asm_task2Assignment1.s
@@ -5,12 +5,12 @@ section .bss			; we define (global) uninitialized variables in .bss section
 	an: resb 33		; enough to store integer in [-2,147,483,648 (-2^31) : 2,147,483,647 (2^31-1)]
 	num: resd 1
 
+section .data
+	index: dd 0
+
 section .text
 	global convertor
 	extern printf
-
-section .data
-	index: dd 0
 
 convertor:
 	push ebp
@@ -18,37 +18,36 @@ convertor:
 	pushad			
 
 	mov ecx, dword [ebp+8]	; get function argument (pointer to string)
-	push an
+	mov eax,0
 	findNum:
-		mov [num], ecx
-		cmp byte [num],'A'
-		jb dig
-		sub byte [num],'A'
-		add byte [num],10
-		jmp convert
-		dig:
-		sub byte [num],'0'
+	push dword [ecx]
+	cmp byte [ecx],'A'
+	jb dig
+	sub byte [ecx],'A'
+	add byte [ecx],10
+	jmp convert
+	dig:
+	sub byte [num],"0"
 	convert:
-		mov edx, 8
-		loop:
-		mov dword ebx, [num]
-		and ebx, edx
-		cmp ebx,0
-		jnz one
-		mov byte [an], '0'
-		jmp cont
-		one:
-		mov byte [an], '1'
-		cont:
-		inc byte [an]
-		shr edx,1
-		cmp edx, 1
-		jnz loop
+	mov edx, 8
+	start: 
+	mov dword ebx, [num]
+	and ebx, edx
+	cmp ebx,0
+	jnz one
+	mov byte [an+eax], "0"
+	jmp cont
+	one:
+	mov byte [an+eax], "1"
+	cont:
+	inc eax
+	shr edx,1
+	cmp edx, 1
+	jnz start
 	inc ecx
 	cmp byte [ecx], 0
 	jnz findNum
-	mov byte [an], 0
-	pop dword [an]
+	;mov byte [an], 0
 	push an				; call printf with 2 arguments -  
 	push format_string	; pointer to str and pointer to format string
 	call printf
