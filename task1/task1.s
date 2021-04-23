82104dfbb01a66ba221773864e3fe5fd99723d23
omergv11 <gevao@post.bgu.ac.il>
2021-04-23 18:29:13 +0300

    commit

--- task1/task1.s
+++ task1/task1.s
@@ -10,18 +10,18 @@ assFunc:
     push ebp              		
     mov ebp, esp         		
     pushad  
-    mov ecx, dword [ebp+8]
+    mov ecx, dword [ebp+8]          
     push ecx
     call c_checkValidity
     add esp,4
-    mov ecx, dword [ebp+8]
+    mov ecx, dword [ebp+8]          ; get function arg again in case c_checkValidity used ecx
     cmp eax, 1
     jne odd
     even:
-    shl ecx, 2
+    shl ecx, 2                      ; mult by 4
     jmp end
     odd:
-    shl ecx, 3
+    shl ecx, 3                      ; mult by 8
     end:
     push ecx
     push form
--- task2/asm_task2Assignment1.s
+++ task2/asm_task2Assignment1.s
@@ -3,10 +3,6 @@ section	.rodata			; we define (global) read-only variables in .rodata section
 
 section .bss			; we define (global) uninitialized variables in .bss section
 	an: resb 33		; enough to store integer in [-2,147,483,648 (-2^31) : 2,147,483,647 (2^31-1)]
-	num: resd 1
-
-section .data
-	index: dd 0
 
 section .text
 	global convertor
@@ -19,21 +15,21 @@ convertor:
 
 	mov ecx, dword [ebp+8]	; get function argument (pointer to string)
 	mov eax,0
-	findNum:
+	findNum:				; find the value of the hex digit
 	mov bh,[ecx]
-	cmp bh,65
-	jb dig
-	sub bh,65
+	cmp bh,'A'				; if the digit is A-F
+	jb num
+	sub bh,'A'
 	add bh,10
 	jmp convert
-	dig:
-	sub bh,48
-	convert:
-	mov bl, 8
+	num:
+	sub bh,'0'				; if the digit is a number 0-9
 	
-	start: 
+	convert:				; start the conversion to binary
+	mov bl, 8
+	start: 					; start of the loop
 	mov dl,bl
-	and bl,bh
+	and bl,bh				; finding the value of each byte of the number	
 	mov byte [an+eax],'0'
 	cmp bl,0
 	je cont
@@ -41,15 +37,14 @@ convertor:
 	cont:
 	inc eax
 	mov bl,dl
-	shr bl,1
-	cmp bl, 0
-	
-	jnz start
+	shr bl,1				; shift bl right so we can address the next byte
+	cmp bl, 0				; check if we reached the last byte
+	jnz start				; if not start the loop again
 	
 	inc ecx
 	cmp byte [ecx], 10
 	jnz findNum
-	mov byte [an+eax+1], 0
+	mov byte [an+eax], 0	; adding 0 so the output will be null terminated
 	push an				; call printf with 2 arguments -  
 	push format_string	; pointer to str and pointer to format string
 	call printf
